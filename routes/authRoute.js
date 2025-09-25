99940eadae48a22c0580f60e30d2d52ae1948fe7
sid-4998 <siddhantjha34@gmail.com>
2025-09-25 20:24:56 +0530

    Added the sign Token and the sign up route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require("../config/prisma");
+const { hashPassword } = require("../utils/bcrypt");
 
 
-module.exports = router;
+router.post("/signup", async (req, res) => {
+    try{
+        const { email, password, name } = req.body;
+
+        const existingUser = await prisma.user.findUnique({ where: { email } });
+        if(existingUser){
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        let tenant = await prisma.tenant.findFirst({ where: { name: name } });
+        if(!tenant) {
+            tenant = await prisma.tenant.create({ data: { name: name } });
+        }
+
+        const hashedPassword = hashPassword(password);
+
+        const user = await prisma.user.create({ 
+            data: {
+                email,
+                password: hashedPassword,
+                role,
+                name,
+                tenantId: tenant.id
+            },
+        });
+
+        res.status(201).json({message: "User created successfully", user});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ error: 'Sign Up failed due to Internal server error' });
+    }
+
+});
+
+module.exports = { router };
--- utils/jwt.js
+++ utils/jwt.js
@@ -4,4 +4,8 @@ function verifyToken(token) {
     return jwt.verify(token, process.env.JWT_SECRET);
 }
 
-module.exports = { verifyToken };
+function signToken(payload) {
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+}
+
+module.exports = { verifyToken, signToken };
